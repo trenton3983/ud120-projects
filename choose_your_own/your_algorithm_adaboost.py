f20f8e5a6990aac5e7ca8f5ddca9f90fa8e1304b
Trenton J. McKinney <trenton_mckinney@yahoo.com>
2018-05-02 18:44:49 -0700

    adding different ML algorithms

--- choose_your_own/your_algorithm_adaboost.py
+++ choose_your_own/your_algorithm_adaboost.py
@@ -3,7 +3,7 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import accuracy_score
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
@@ -12,10 +12,10 @@ features_train, labels_train, features_test, labels_test = makeTerrainData()
 # the training data (features_train, labels_train) have both "fast" and "slow"
 # points mixed together--separate them so we can give them different colors
 # in the scatterplot and identify them visually
-grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==0]
-bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
-grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
-bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii] == 0]
+bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii] == 0]
+grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii] == 1]
+bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii] == 1]
 
 
 # initial visualization
@@ -33,7 +33,7 @@ plt.show()
 # visualization code (prettyPicture) to show you the decision boundary
 
 
-clf = KNeighborsClassifier(n_neighbors=4)
+clf = AdaBoostClassifier(n_estimators=50)
 
 clf.fit(features_train, labels_train)
 
@@ -42,7 +42,6 @@ pred = clf.predict(features_test)
 acc = accuracy_score(pred, labels_test)
 
 print(clf.score(features_test, labels_test))
-
 print(f'Accuracy: {acc}')
 
 try:
