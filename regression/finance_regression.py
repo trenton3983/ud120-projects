a8160d11a362a4a071d15a545d34a5479be0cdb7
Trenton J. McKinney <trenton_mckinney@yahoo.com>
2018-05-22 21:29:20 -0700

    updated to work in python 3.6 with pathlib
    added regression, slope, intercept and r^2

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -12,29 +12,46 @@
 
 
 import pickle
+from pathlib import Path
 from feature_format import featureFormat, targetFeatureSplit
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
 
-dictionary = pickle.load(open("../final_project/final_project_dataset_modified.pkl", "r"))
+sort_keys_dir = Path(__file__).parents[1].joinpath('tools')
+sort_keys_file_name = 'python2_lesson06_keys_unix.pkl'
+sort_keys_file_name = sort_keys_dir.joinpath(sort_keys_file_name)
+print(f'Sort Keys: {sort_keys_file_name}')
+print(f'Sort Type: {type(sort_keys_file_name)}')
+
+location_dir = Path(__file__).parents[1].joinpath('final_project')
+file_name = 'final_project_dataset_modified_unix.pkl'
+file_name = location_dir.joinpath(file_name)
+print(f'Data Path: {file_name}')
+
+dictionary = pickle.load(open(file_name, "rb"))
 
 # list the features you want to look at--first item in the
 # list will be the "target" feature
 features_list = ["bonus", "salary"]
-data = featureFormat(dictionary, features_list, remove_any_zeroes=True)
+data = featureFormat(dictionary, features_list, remove_any_zeroes=True, sort_keys=sort_keys_file_name, _print=True)
 target, features = targetFeatureSplit(data)
 
 # training-testing split needed in regression, just like classification
 
-feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
+feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5,
+                                                                          random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 # Your regression goes here!
 # Please name it reg, so that the plotting code below picks it up and
 # plots it correctly. Don't forget to change the test_color above from "b" to
 # "r" to differentiate training points from test points.
+
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
 
 # draw the scatterplot, with color-coded training and testing points
 
@@ -46,6 +63,15 @@ for feature, target in zip(feature_train, target_train):
 # labels for the legend
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
 plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
+
+
+print(f'Slope: {reg.coef_}')
+print(f'Intercept: {reg.intercept_}')
+
+print('\n########## Stats on Test Dataset ##########')
+print(f'r-squared score: {reg.score(feature_test, target_test)}')
+print('\n########## Stats on Training Dataset ##########')
+print(f'r-squared score: {reg.score(feature_train, target_train)}\n')
 
 # draw the regression line, once it's coded
 try:
