0d482fc179afbb21afcf9d574a67369cf54e50e4
Trenton J. McKinney <trenton_mckinney@yahoo.com>
2018-06-06 16:05:52 -0700

    added printing and changed directory / path

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python
 
 import pickle
-import sys
-import matplotlib.pyplot
-sys.path.append("../tools/")
+import matplotlib.pyplot as plt
 from feature_format import featureFormat, targetFeatureSplit
+from pathlib import Path
 
 
-### read in data dictionary, convert to numpy array
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+# read in data dictionary, convert to numpy array
+data_dict_dir = Path(__file__).parents[1].joinpath('final_project')
+data_dict_file = 'final_project_dataset_unix.pkl'
+data_dict_file = data_dict_dir.joinpath(data_dict_file)
+data_dict = pickle.load(open(data_dict_file, "rb"))
 features = ["salary", "bonus"]
-data = featureFormat(data_dict, features)
+data = featureFormat(data_dict, features, _print=True)
 
 
-### your code below
+# your code below
+
+plt.scatter(data[:-1, 0], data[:-1, 1])
+plt.xlabel('Salary')
+plt.ylabel('Bonus')
+plt.savefig('enron_outliers.png')
+plt.show()
 
 
 
