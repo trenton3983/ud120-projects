19889f9cdeb92d64e780ef652037ae407a4eece1
Trenton J. McKinney <trenton3983@users.noreply.github.com>
2018-06-21 16:03:08 -0700

    finished code for quizzes

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -5,28 +5,24 @@
 """
 
 
-
-
 import pickle
-import numpy
+import pandas as pd
 import matplotlib.pyplot as plt
-import sys
-sys.path.append("../tools/")
+from pathlib import Path
 from feature_format import featureFormat, targetFeatureSplit
-
-
+from sklearn.cluster import KMeans
 
 
 def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
     """ some plotting code designed to help you visualize your clusters """
 
-    ### plot each cluster with a different color--add more colors for
-    ### drawing more than five clusters
+    # plot each cluster with a different color--add more colors for
+    # drawing more than five clusters
     colors = ["b", "c", "k", "m", "g"]
     for ii, pp in enumerate(pred):
-        plt.scatter(features[ii][0], features[ii][1], color = colors[pred[ii]])
+        plt.scatter(features[ii][0], features[ii][1], color=colors[pred[ii]])
 
-    ### if you like, place red stars over points that are POIs (just for funsies)
+    # if you like, place red stars over points that are POIs (just for funsies)
     if mark_poi:
         for ii, pp in enumerate(pred):
             if poi[ii]:
@@ -37,40 +33,89 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
     plt.show()
 
 
+# read in data dictionary, convert to numpy array
+data_dict_dir = Path(__file__).parents[1].joinpath('final_project')
+data_dict_file = 'final_project_dataset_unix.pkl'
+data_dict_file = data_dict_dir.joinpath(data_dict_file)
+data_dict = pickle.load(open(data_dict_file, "rb"))
 
-### load in the dict of dicts containing all the data on each person in the dataset
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
-### there's an outlier--remove it! 
+# there's an outlier--remove it!
 data_dict.pop("TOTAL", 0)
 
 
-### the input features we want to use 
-### can be any key in the person-level dictionary (salary, director_fees, etc.) 
+# the input features we want to use
+# can be any key in the person-level dictionary (salary, director_fees, etc.)
+all_feature_list = []
+for k, v in data_dict.items():
+    for k1, v1 in v.items():
+        if k1 not in all_feature_list:
+            all_feature_list.append(k1)
+print('\nAll possible features:\n', all_feature_list)
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
-poi  = "poi"
-features_list = [poi, feature_1, feature_2]
-data = featureFormat(data_dict, features_list )
-poi, finance_features = targetFeatureSplit( data )
+feature_3 = 'total_payments'
+poi = "poi"
+num_clusters = 2  # number of clusters for KMeans
+feature_count = 2  # 2 or 3
+
+if feature_count == 2:
+    # 2 features
+    features_list = [poi, feature_1, feature_2]
+    data = featureFormat(data_dict, features_list)
+    poi, finance_features = targetFeatureSplit(data)
+    for f1, f2 in finance_features:
+        plt.scatter(f1, f2)
+
+elif feature_count == 3:
+    # 3 features
+    features_list = [poi, feature_1, feature_2, feature_3]
+    data = featureFormat(data_dict, features_list)
+    poi, finance_features = targetFeatureSplit(data)
+    for f1, f2, _ in finance_features:
+        plt.scatter(f1, f2)
 
 
-### in the "clustering with 3 features" part of the mini-project,
-### you'll want to change this line to 
-### for f1, f2, _ in finance_features:
-### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
-    plt.scatter( f1, f2 )
+plt.title(f'{num_clusters} clusters & {feature_count} features')
+plt.xlabel(feature_1)
+plt.ylabel(feature_2)
+plt.savefig('features_scatter.png')
 plt.show()
 
-### cluster here; create predictions of the cluster labels
-### for the data and store them to a list called pred
+# cluster here; create predictions of the cluster labels
+# for the data and store them to a list called pred
 
+pred = KMeans(n_clusters=num_clusters).fit_predict(finance_features)
 
-
-
-### rename the "name" parameter when you change the number of features
-### so that the figure gets saved to a different file
+# rename the "name" parameter when you change the number of features
+# so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=True,
+         name=f"{str(num_clusters)}_clusters_{str(feature_count)}_features.png", f1_name=feature_1, f2_name=feature_2)
 except NameError:
-    print "no predictions object named pred found, no clusters to plot"
+    print("no predictions object named pred found, no clusters to plot")
+
+
+def extract_data_dict(feature: str, d_dict: dict=data_dict) -> list:
+    """
+    Given the data_dict and the feature name, return a list of values excluding NaN
+    :param feature: name of the feature
+    :param d_dict:
+    :return: list of feature values
+    """
+    return [v[feature] for k, v in d_dict.items() if v[feature] != 'NaN']
+
+
+exercised_stock_options_list = extract_data_dict(feature='exercised_stock_options')
+print(f'\nMin of exercised_stock_options: {min(exercised_stock_options_list)}')
+print(f'\nMax of exercised_stock_options: {max(exercised_stock_options_list)}')
+
+salary_list = extract_data_dict(feature='salary')
+print(f'\nMin of salary: {min(salary_list)}')
+print(f'\nMax of salary: {max(salary_list)}\n')
+
+
+# data_dict my also be loaded into a pandas DataFrame
+print('\nWith a Pandas DataFrame:')
+data_dict_df = pd.DataFrame(data_dict)
+print(data_dict_df)
+print('\nMin exercised_stock_options: ', min([v for v in data_dict_df.loc['exercised_stock_options'] if v != 'NaN']))
