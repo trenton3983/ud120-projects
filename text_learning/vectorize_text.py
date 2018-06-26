bb18b24c4b81e836e1296548fc6f80f6ae27d921
Trenton J. McKinney <trenton3983@users.noreply.github.com>
2018-06-26 14:36:26 -0700

    added code for quiz Clean Away "Signature Words"

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -3,9 +3,8 @@
 import os
 import pickle
 import re
-import sys
 
-sys.path.append( "../tools/" )
+
 from parse_out_email_text import parseOutText
 
 """
@@ -23,50 +22,64 @@ from parse_out_email_text import parseOutText
 """
 
 
-from_sara  = open("from_sara.txt", "r")
+from_sara = open("from_sara.txt", "r")
 from_chris = open("from_chris.txt", "r")
 
 from_data = []
 word_data = []
 
-### temp_counter is a way to speed up the development--there are
-### thousands of emails from Sara and Chris, so running over all of them
-### can take a long time
-### temp_counter helps you only look at the first 200 emails in the list so you
-### can iterate your modifications quicker
+# temp_counter is a way to speed up the development--there are
+# thousands of emails from Sara and Chris, so running over all of them
+# can take a long time
+# temp_counter helps you only look at the first 200 emails in the list so you
+# can iterate your modifications quicker
 temp_counter = 0
 
 
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
-        ### only look at first 200 emails when developing
-        ### once everything is working, remove this line to run over full dataset
+        path = path[:-2] + '_'
+        print(path)
+        # only look at first 200 emails when developing
+        # once everything is working, remove this line to run over full dataset
         temp_counter += 1
         if temp_counter < 200:
-            path = os.path.join('..', path[:-1])
-            print path
+            path = os.path.join('C:/Data/', path)
+            print(path)
             email = open(path, "r")
 
-            ### use parseOutText to extract the text from the opened email
+            # use parseOutText to extract the text from the opened email
+            text = parseOutText(email)
 
-            ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
+            # use str.replace() to remove any instances of the words
+            remove = ["sara", "shackleton", "chris", "germani"]
 
-            ### append the text to word_data
+            text_cleaned = [item for item in text if item not in remove]
+            text_cleaned = ' '.join(text_cleaned)
 
-            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+            # append the text to word_data
+            word_data.append(text_cleaned)
+
+            # append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+            if name == 'sara':
+                from_data.append(0)
+            elif name == 'chris':
+                from_data.append(1)
 
 
             email.close()
 
-print "emails processed"
+print("emails processed")
 from_sara.close()
 from_chris.close()
 
-pickle.dump( word_data, open("your_word_data.pkl", "w") )
-pickle.dump( from_data, open("your_email_authors.pkl", "w") )
+pickle.dump(word_data, open("your_word_data.pkl", "wb"))
+pickle.dump(from_data, open("your_email_authors.pkl", "wb"))
 
+print(word_data)
+print(from_data)
 
+print(word_data[152])
 
 
 
